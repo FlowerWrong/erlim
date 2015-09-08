7a39c8754de8f5b8d290bcda499daf3a465ec430
flowerwrong <sysuyangkang@gmail.com>
2015-09-08 11:05:37 +0800

    Fix client send msg with only one socket

--- src/ck.erl
+++ src/ck.erl
@@ -1,23 +1,22 @@
 -module(ck).
 
--export([login/0, sc/0, gc/0, logout/0]).
+-export([login/0, sc/1, gc/1, logout/1]).
 
 login() ->
     {ok, Sock} = gen_tcp:connect("localhost", 3000, [binary, {packet, 0}]),
     Msg = <<"{\"cmd\": \"login\", \"username\": \"kang\", \"password\": \"123456\"}">>,
-    ok = gen_tcp:send(Sock, Msg).
+    ok = gen_tcp:send(Sock, Msg),
+    Sock.
 
-sc() ->
-    {ok, Sock} = gen_tcp:connect("localhost", 3000, [binary, {packet, 0}]),
+sc(Sock) ->
     Msg = <<"{\"cmd\": \"single_chat\", \"username\": \"kang\", \"password\": \"123456\", \"to\": \"kang\", \"msg\": \"hello world\"}">>,
     ok = gen_tcp:send(Sock, Msg).
 
-gc() ->
-    {ok, Sock} = gen_tcp:connect("localhost", 3000, [binary, {packet, 0}]),
+gc(Sock) ->
     Msg = <<"{\"cmd\": \"group_chat\", \"username\": \"kang\", \"password\": \"123456\", \"to\": \"xiaomiroom\", \"msg\": \"hello world, xiaomi\"}">>,
     ok = gen_tcp:send(Sock, Msg).
 
-logout() ->
-    {ok, Sock} = gen_tcp:connect("localhost", 3000, [binary, {packet, 0}]),
+logout(Sock) ->
     Msg = <<"{\"cmd\": \"logout\", \"username\": \"kang\", \"password\": \"123456\"}">>,
-    ok = gen_tcp:send(Sock, Msg).
+    ok = gen_tcp:send(Sock, Msg),
+    ok = gen_tcp:close(Sock).
