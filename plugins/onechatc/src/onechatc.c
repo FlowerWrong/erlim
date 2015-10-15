497a278323bf3990a6601c5e6540cf8eea79c8f1
flowerwrong <sysuyangkang@gmail.com>
2015-10-15 10:21:11 +0800

    Add cmake list

--- plugins/onechatc/src/onechatc.c
+++ plugins/onechatc/src/onechatc.c
@@ -17,17 +17,18 @@
 #define BUFFER_SIZE 1024
 
 int main() {
-	///定义sockfd
+	// 定义sockfd
 	int sock_cli = socket(AF_INET, SOCK_STREAM, 0);
 
-	///定义sockaddr_in
+	// 定义sockaddr_in
 	struct sockaddr_in servaddr;
+	// memset() 函数常用于内存空间初始化
 	memset(&servaddr, 0, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(MYPORT);  ///服务器端口
-	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");  ///服务器ip
+	servaddr.sin_port = htons(MYPORT);  // 服务器端口
+	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");  // 服务器ip
 
-	///连接服务器，成功返回0，错误返回-1
+	// 连接服务器，成功返回0，错误返回-1
 	if (connect(sock_cli, (struct sockaddr *) &servaddr, sizeof(servaddr))
 			< 0) {
 		perror("connect");
@@ -37,10 +38,10 @@ int main() {
 	char sendbuf[BUFFER_SIZE];
 	char recvbuf[BUFFER_SIZE];
 	while (fgets(sendbuf, sizeof(sendbuf), stdin) != NULL) {
-		send(sock_cli, sendbuf, strlen(sendbuf), 0); ///发送
+		send(sock_cli, sendbuf, strlen(sendbuf), 0); // 发送
 		if (strcmp(sendbuf, "exit\n") == 0)
 			break;
-		recv(sock_cli, recvbuf, sizeof(recvbuf), 0); ///接收
+		recv(sock_cli, recvbuf, sizeof(recvbuf), 0); // 接收
 		fputs(recvbuf, stdout);
 
 		memset(sendbuf, 0, sizeof(sendbuf));
